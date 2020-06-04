e41a60765d186a09093213d6e8018d07f020e817
Jae P <jp.ark@outlook.com>
2020-06-04 12:21:47 -0700

    Wrote function bodies and updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions makeCacheMatrix and cacheSolve will together utilize lexical scoping of
+## mechanism of R in order to store an evaluated inverse of a given solvable matrix, so that the
+## inverse can be retrieved without having to go through calculation which can be costly.
 
-## Write a short comment describing this function
+## makeCacheMatrix function will store in its environment the inverse of the given matrix. The
+## created object from makeCacheMatrix will contain the following functions in order to assign and
+## retrieve the matrix and its inverse: set(), get(), setinverse() and getinverse().
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(calc) inv <<- calc
+    getinverse <- function() inv
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Given an instance of makeCacheMatrix function, cacheSolve utilizes the getter and setter
+## functions in order to calculate and store the inverse of the matrix set in makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    mtx <- x$get()
+    inv <- solve(mtx)
+    x$setinverse(inv)
+    inv
 }
